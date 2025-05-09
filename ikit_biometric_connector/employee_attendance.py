7cc95659ef84ae29f04b50d7e22813d33bc55dde
Malik <shahid03351206668@gmail.com>
2025-05-09 15:45:51 +0000

    prime backup

--- ikit_biometric_connector/employee_attendance.py
+++ ikit_biometric_connector/employee_attendance.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 import frappe
-from frappe.utils import cint
+from frappe.utils import cint, cstr
 from frappe.utils.background_jobs import enqueue
 import json
 
@@ -12,15 +12,22 @@ def generate_attendance_long(data= None):
 		return "Queued"
 	except Exception as e:
 		error_message = f"""{frappe.get_traceback()}\n{attendance}\n{e}"""
-		frappe.log_error(error_message, "Error Exception Queque")
+		frappe.log_error("Error Exception Queque", error_message)
+
+
+def clean_string_value(val):
+	if val:
+		return cstr(val).replace(",", "").replace("(", "").replace(")", "").strip()
+	return val
 
 @frappe.whitelist(allow_guest=True)
 def generate_attendance(attendance = None):
 	try:
 		attendance = json.loads(attendance).get("data")
+		frappe.log_error("Attendance Data From app", cstr(attendance))
 		for a  in attendance:
 			a = str(a).split()
-			biometric, date, time, in_out= str(a[1]), a[3], a[4], cint(a[6].replace(")", ""))
+			biometric, date, time, in_out_1, in_out_2 = str(a[1]), a[3], a[4], cint(clean_string_value(a[5])), cint(clean_string_value(a[6]))
 			date_time = f"""{date} {time}"""
 			emp = frappe.db.get_value("Employee", {"status": "Active", "attendance_device_id": biometric}, "name")
 			if emp:
@@ -30,12 +37,17 @@ def generate_attendance(attendance = None):
 						new_att = frappe.new_doc("Employee Checkin")
 						new_att.employee = emp
 						new_att.time = date_time
-						new_att.log_type = "OUT" if in_out else "IN"
+						log_type =  None
+						if in_out_1 == 0 and in_out_2 == 0:
+							log_type = "IN" 
+						elif in_out_1 == 0 and in_out_2 == 1:
+							log_type = "OUT" 
+						new_att.log_type = log_type
 						new_att.flags.ignore_permissions=True
 						new_att.save()
 					except Exception as e:
 						error_message = f"""{frappe.get_traceback()}\n{a}\n{e}"""
-						frappe.log_error(error_message, "Checkin Creation Error from Mobile App")
+						frappe.log_error("Checkin Creation Error from Mobile App", error_message)
 	except Exception as e:
 		error_message = f"""{frappe.get_traceback()}\n{attendance}\n{e}"""
-		frappe.log_error(error_message, "Generate Attendance Error from Mobile App")
+		frappe.log_error("Generate Attendance Error from Mobile App", error_message)
